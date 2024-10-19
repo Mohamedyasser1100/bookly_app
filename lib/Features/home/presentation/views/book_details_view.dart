4e2ddd5015a826ec18f36736e1c37373393a1a8b
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-10-19 18:04:56 +0300

    create book details view

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/home/presentation/views/widget/book_details_view_bode.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBode(),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widget/book_details_view_bode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBode extends StatelessWidget {
+  const BookDetailsViewBode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
