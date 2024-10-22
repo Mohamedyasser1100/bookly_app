79a035c050fc2a3a6a832e52f57b9d693ddd8964
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-10-22 19:35:58 +0300

    create book model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/constans.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
