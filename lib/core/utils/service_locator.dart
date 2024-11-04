6e3b73ff106a9ee4cfd235a582cb84a02c10054a
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-11-04 22:38:20 +0200

    setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_impelementation.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void SetupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpelementation>(
+    HomeRepoImpelementation(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_impelementation.dart';
+import 'package:bookly_app/Features/home/presentation/views/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/views/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/constans.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SetupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,14 +18,27 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      //navigation in our app
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        //حطيت نوع الخط وبعد ما احط نوع الخط بخلي الثيم دارك
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FeaturedBooksCubit(getIt.get<HomeRepoImpelementation>()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              NewestBooksCubit(getIt.get<HomeRepoImpelementation>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        //navigation in our app
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          //حطيت نوع الخط وبعد ما احط نوع الخط بخلي الثيم دارك
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
