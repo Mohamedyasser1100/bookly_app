354213d1d58c17f5be82108a2a25f86a8ad89e61
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-11-09 17:53:06 +0200

    fetch similar books done

--- lib/Features/home/presentation/views/widget/book_details_view_bode.dart
+++ lib/Features/home/presentation/views/widget/book_details_view_bode.dart
@@ -1,10 +1,11 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/books_details_section.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/custome_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBode extends StatelessWidget {
-  const BookDetailsViewBode({super.key});
+  const BookDetailsViewBode({super.key, required BookModel bookModel});
 
   @override
   Widget build(BuildContext context) {
--- lib/Features/home/presentation/views/widget/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widget/similar_books_list_view.dart
@@ -1,25 +1,39 @@
+import 'package:bookly_app/Features/home/presentation/views/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/custome_book_image.dart';
+import 'package:bookly_app/core/widgets/custome_error_widget.dart';
+import 'package:bookly_app/core/widgets/custome_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.15,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5),
-            child: CustomeBookImage(
-              imageUrl:
-                  'https://www.google.com/imgres?q=photo%20books&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F719WTtlFxTL._AC_UF894%2C1000_QL80_.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.eg%2F-%2Fen%2FNvhai-Library-Painting-Reading-Artwork%2Fdp%2FB09CM6YJYS&docid=YXVHj4VccIjSLM&tbnid=kigcwD1xAcyzVM&vet=12ahUKEwjc9vu-pc-JAxXiQEEAHVedC-wQM3oECH8QAA..i&w=846&h=1000&hcb=2&ved=2ahUKEwjc9vu-pc-JAxXiQEEAHVedC-wQM3oECH8QAA',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: CustomeBookImage(
+                    imageUrl:
+                        'https://www.google.com/imgres?q=photo%20books&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F719WTtlFxTL._AC_UF894%2C1000_QL80_.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.eg%2F-%2Fen%2FNvhai-Library-Painting-Reading-Artwork%2Fdp%2FB09CM6YJYS&docid=YXVHj4VccIjSLM&tbnid=kigcwD1xAcyzVM&vet=12ahUKEwjc9vu-pc-JAxXiQEEAHVedC-wQM3oECH8QAA..i&w=846&h=1000&hcb=2&ved=2ahUKEwjc9vu-pc-JAxXiQEEAHVedC-wQM3oECH8QAA',
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomeErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomeLoadingIndicator();
+        }
+      },
     );
   }
 }
